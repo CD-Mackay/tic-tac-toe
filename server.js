b5d3761421dd4f429488c21c2e91d904d94009a2
Connor Mackay <connor.mackay@gmail.com>
2021-08-01 19:48:54 -0400

    render homepage

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'html');
+
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+ })
  
-app.listen(3000);
+app.listen(3000, console.log(`server started on port 3000`));
